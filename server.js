6006e80bdbce7afac68c76d96350657e1b997929
danny huang <dannyhuang1234@hotmail.com>
2022-03-27 18:12:44 +1100

    WIP update + submit express body parser setup done, TODO create a store

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
 
 const app = express();
 const port = process.env.PORT || 8080;
+
+
+// var React = require('react');
+// // Our bundle expects React to be a global
+// global.React = React;
+// var hCardComponent = require('./public/main.js').default;
+
+
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
 
 //serving up files in public 
 app.use(express.static("public"));
@@ -11,6 +22,16 @@ app.use(express.static("public"));
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '/index.html'));
 });
+
+app.post('/update',(request,response) => {
+    console.log('updated data',request.body);
+    response.send('POST request update success');
+})
+
+app.post('/submit',(request,response) => {
+    console.log('submitted data',request.body);
+    response.send('User Successfully updated');
+})
 
 app.listen(port);
 console.log('Server started at http://localhost:' + port);
