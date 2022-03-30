b15e285bce2b40570011aeaae5b73890511950d2
danny huang <dannyhuang1234@hotmail.com>
2022-03-30 14:17:34 +1100

    WIP holding

--- src/index.jsx
+++ src/index.jsx
@@ -1,21 +1,26 @@
 
-var hCardProps = {
-    givenName: 'Sam',
-    surname: 'Fairfax',
-    email: 'sam.fairfax@fairfaxmedia.com.au',
-    phone: '0292822833',
-    houseNumber: '100',
-    street: 'Harris Street',
-    suburb: 'Pyrmont',
-    state: 'NSW',
-    postcode: '2009',
-    country: 'Australia'
-  };
 
+// //here we will call our reducer and get the saved data
+// import { createRequire } from 'module';
+// const require = createRequire(import.meta.url);
+// const { Provider } = require("./data/context");
 
-//here we will call our reducer and get the saved data
-
+// console.log('line 7',Provider);
+// let hCardProps = {
+//   givenName: "Sam",
+//   surname: "Fairfax",
+//   email: "sam.fairfax@fairfaxmedia.com.au",
+//   phone: "0292822833",
+//   houseNumber: "100",
+//   street: "Harris Street",
+//   suburb: "Pyrmont",
+//   state: "NSW",
+//   postcode: "2009",
+//   country: "Australia",
+// };
+let hCardPropsData = JSON.stringify(hCardProps)
+console.log('line22',hCardPropsData);
 ReactDOM.render(
-    React.createElement(window.hCard.default, hCardProps),
+    React.createElement(window.hCard.default, hCardPropsData),
     document.querySelector(".HcardApp")
 );
--- src/server.js
+++ src/server.js
@@ -15,7 +15,7 @@ const {save} = require('./save');
 // var React = require('react');
 // // Our bundle expects React to be a global
 // global.React = React;
-// var hCardComponent = require('./public/main.js').default;
+// var hCardComponent = require('../main.js').default;
 
 
 
@@ -26,16 +26,27 @@ const port = process.env.PORT || 8080;
 
 
 
-// var React = require('react');
-// // Our bundle expects React to be a global
-// global.React = React;
-// var hCardComponent = require('./public/main.js').default;
+var React = require('react');
+// Our bundle expects React to be a global
+global.React = React;
+var hCardComponent = require('./main.js').default;
 
 // const save = (props) => {
 //     console.log('hello',props);
 // };
 
-
+var hCardProps = {
+  givenName: "Sam",
+  surname: "Fairfax",
+  email: "sam.fairfax@fairfaxmedia.com.au",
+  phone: "0292822833",
+  houseNumber: "100",
+  street: "Harris Street",
+  suburb: "Pyrmont",
+  state: "NSW",
+  postcode: "2009",
+  country: "Australia"
+};
 
 
 app.use(bodyParser.urlencoded());
@@ -44,9 +55,16 @@ app.use(bodyParser.json());
 //serving up files in public 
 app.use(express.static("public"));
 
+//passing to html
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
 // sendFile will go here
 app.get('*', function(req, res) {
-  res.sendFile(path.join(__dirname, '/index.html'));
+  // res.sendFile(path.join(__dirname, '/index.html'));
+  res.render(path.join(__dirname, 'index'),{
+    hCardProps : hCardProps
+  });
 });
 
 app.post('/update',(request,response) => {
