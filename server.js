d699f3d4e3f74f39ee6d58394406d796d7fb210a
Xascrimson <35974973+Xascrimson@users.noreply.github.com>
2022-03-30 23:21:05 +1300

    Feature/injection (#1)
    * index.pug created + data able to be passed to html
    
    * server comments

--- server.js
+++ server.js
@@ -1,29 +1,25 @@
-const express = require('express');
-const path = require('path');
-const bodyParser = require('body-parser');
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
 
-const {save} = require('./save');
+const { save } = require("./save");
+
+let { userData } = require("./store");
 
 // import express from 'express';
 // import path from 'path';
 // import bodyParser from 'body-parser';
 // const __dirname = path.resolve();
 
-
-
-var React = require('react');
-// Our bundle expects React to be a global
-global.React = React;
-var hCardComponent = require('./public/main.js').default;
-
-
+// var React = require('react');
+// // Our bundle expects React to be a global
+// global.React = React;
+// var hCardComponent = require('./public/main.js').default;
 
 const app = express();
 const port = process.env.PORT || 8080;
 
-
-
-
+app.set("view engine", "pug");
 
 // var React = require('react');
 // // Our bundle expects React to be a global
@@ -34,30 +30,27 @@ const port = process.env.PORT || 8080;
 //     console.log('hello',props);
 // };
 
-
-
-
 app.use(bodyParser.urlencoded());
 app.use(bodyParser.json());
 
-//serving up files in public 
+//serving up files in public
 app.use(express.static("public"));
 
 // sendFile will go here
-app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, '/index.html'));
+app.get("*", function (req, res) {
+    res.render("index.pug", { data: JSON.stringify(userData) });
 });
 
-app.post('/update',(request,response) => {
-    console.log('updated data',request.body);
-    save(request);
-    response.send('POST request update success');
-})
+//POST update, updating userData based on input selection
+app.post("/update", (request, response) => {
+    userData = save(request, userData);
+    response.send("POST request update success");
+});
 
-app.post('/submit',(request,response) => {
-    console.log('submitted data',request.body);
-    response.send('User Successfully updated');
-})
+//POST submit, update hcard with new user data, + future can send to parameter store etc, so all servers update with new data.
+app.post("/submit", (request, response) => {
+    response.redirect("/");
+});
 
 app.listen(port);
-console.log('Server started at http://localhost:' + port);
+console.log("Server started at http://localhost:" + port);
---
+++ store.js
@@ -0,0 +1,14 @@
+let userData = {
+    givenName: "Sam",
+    surname: "Fairfax",
+    email: "sam.fairfax@fairfaxmedia.com.au",
+    phone: "0292822833",
+    houseNumber: "100",
+    street: "Harris Street",
+    suburb: "Pyrmont",
+    state: "NSW",
+    postcode: "2009",
+    country: "Australia",
+};
+
+module.exports = {userData};
